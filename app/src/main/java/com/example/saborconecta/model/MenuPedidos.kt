c99a88f948dce4779b88be0ef1752629598e4215
Cleverson Silva <cleversonps10@hotmail.com>
2024-05-02 23:10:29 -0300

    Commit 03

---
+++ app/src/main/java/com/example/saborconecta/model/MenuPedidos.kt
@@ -0,0 +1,7 @@
+package com.example.saborconecta.model
+
+data class MenuPedidos(
+    val NumeroPedido: String,
+    val Agrofamiliar: String,
+    val Nome_Produto: String,
+    val Preco: String)
